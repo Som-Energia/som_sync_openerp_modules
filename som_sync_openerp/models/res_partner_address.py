6478397f09d820c0e43abb5f8202ed43aae0c500
pedropote <pere.montagud@somenergia.coop>
2026-01-20 17:03:18 +0100

    🚧 WIP res.partner.address sync

--- som_sync_openerp/models/res_partner_address.py
+++ som_sync_openerp/models/res_partner_address.py
@@ -9,28 +9,39 @@ class ResPartnerAddress(osv.osv):
     MAPPING_FIELDS_TO_SYNC = {
         'id': 'pnt_erp_id',
         'name': 'name',
-        # 'type': 'type', not mapped, is a constant
         'email': 'email',
         'phone': 'phone',
         'street': 'street',
         'zip': 'code_zip',
+        'partner_id': 'parent_id',
+        'state_id': 'state_id',
+        'country_id': 'country_id',
     }
-    # TODO: Add foreign keys when API doc is available
     MAPPING_FK = {
-        'municipi_id': 'res.municipi',
+        'state_id': 'res.country.state',
+        'country_id': 'res.country',
         'partner_id': 'res.partner',
     }
 
     MAPPING_CONSTANTS = {
         'type': 'invoice',
         'is_company': False,
+        # TODO: Not lang in partner.address, worthit get it from partner?
+        'lang': 'ca_ES',
     }
 
     def get_endpoint_suffix(self, cr, uid, id, context={}):
+        # /contact/{parent_id}/{ttype}
         address = self.browse(cr, uid, id, context=context)
-        if address.partner_id.vat:
-            # TODO: check with API doc when available
-            res = 'other/{}'.format(address.partner_id.vat)
+        sync_obj = self.pool.get('odoo.sync')
+        sync_parent_ids = sync_obj.search(cr, uid, [
+            ('model', '=', 'res.partner'),
+            ('res_id', '=', address.partner_id.id),
+        ])
+        if sync_parent_ids:
+            parent_odoo_id = sync_obj.read(
+                cr, uid, sync_parent_ids[0], ['odoo_id'])['odoo_id']
+            res = 'contact/{}/{}'.format(parent_odoo_id, 'invoice')
             return res
         else:
             return False
@@ -38,9 +49,9 @@ class ResPartnerAddress(osv.osv):
     def create(self, cr, uid, vals, context={}):
         ids = super(ResPartnerAddress, self).create(cr, uid, vals, context=context)
 
-        sync_obj = self.pool.get('odoo.sync')
-        sync_obj.syncronize_sync(
-            cr, uid, self._name, 'create', ids, context=context)
+        # sync_obj = self.pool.get('odoo.sync')
+        # sync_obj.syncronize_sync(
+        #     cr, uid, self._name, 'create', ids, context=context)
 
         return ids
 
