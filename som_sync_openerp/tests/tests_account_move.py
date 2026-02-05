dd2bbd39c4ea01167273e349b3fb1a15b4a5bbe7
Oriol Piera <oriol.piera@somenergia.coop>
2026-02-05 08:09:11 +0100

    ✅ account.move write triggers sync when journal sync_odoo is enabled

--- som_sync_openerp/tests/tests_account_move.py
+++ som_sync_openerp/tests/tests_account_move.py
@@ -1,6 +1,7 @@
 
 # -*- coding: utf-8 -*-
 from destral import testing
+from mock import MagicMock
 
 
 class TestAccountMove(testing.OOTestCaseWithCursor):
@@ -8,6 +9,7 @@ class TestAccountMove(testing.OOTestCaseWithCursor):
     def setUp(self):
         self.am_obj = self.openerp.pool.get("account.move")
         self.imd_obj = self.openerp.pool.get("ir.model.data")
+        self.sync_obj = self.openerp.pool.get("odoo.sync")
         super(TestAccountMove, self).setUp()
 
     def test__journal_is_syncrozable_True(self):
@@ -31,3 +33,63 @@ class TestAccountMove(testing.OOTestCaseWithCursor):
         )
 
         self.assertFalse(is_syncrozable)
+
+    def test__write_triggers_async(self):
+        move_id = self.imd_obj.get_object_reference(
+            self.cursor, self.uid, "som_sync_openerp", "account_move_001"
+        )[1]
+        # sync_model_enabled_amplified returns (sync_enabled, auto_sync, async_enabled)
+        self.sync_obj.sync_model_enabled_amplified = MagicMock(
+            return_value=(True, True, True))
+        self.sync_obj.syncronize = MagicMock()
+
+        # Perform write operation
+        self.am_obj.write(
+            self.cursor,
+            self.uid,
+            [move_id],
+            {'state': 'posted'},
+        )
+
+        self.sync_obj.syncronize.assert_called_once()
+
+    def test__write_no_triggers_async_journal_disabled_sync(self):
+        move_id = self.imd_obj.get_object_reference(
+            self.cursor, self.uid, "som_sync_openerp", "account_move_002"
+        )[1]
+        # sync_model_enabled_amplified returns (sync_enabled, auto_sync, async_enabled)
+        self.sync_obj.sync_model_enabled_amplified = MagicMock(
+            return_value=(True, True, True))
+        self.sync_obj.syncronize = MagicMock()
+
+        # Perform write operation
+        self.am_obj.write(
+            self.cursor,
+            self.uid,
+            [move_id],
+            {'state': 'posted'},
+        )
+
+        self.sync_obj.syncronize.assert_not_called()
+
+    def test__write__autosync_not_enabled_no_trigger(self):
+        move_id = self.imd_obj.get_object_reference(
+            self.cursor, self.uid, "som_sync_openerp", "account_move_001"
+        )[1]
+        # sync_model_enabled_amplified returns (sync_enabled, auto_sync, async_enabled)
+        self.sync_obj.sync_model_enabled_amplified = MagicMock(
+            return_value=(True, False, False))
+        self.sync_obj.syncronize = MagicMock()
+        self.sync_obj.syncronize_sync = MagicMock()
+
+        # Perform write operation on a field that does not trigger sync
+        self.am_obj.write(
+            self.cursor,
+            self.uid,
+            [move_id],
+            {'state': 'posted'},
+        )
+
+        # Assert that the sync method was not called
+        self.sync_obj.syncronize.assert_not_called()
+        self.sync_obj.syncronize_sync.assert_not_called()
